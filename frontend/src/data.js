a68d5ed8e8440f030e4959152af0fb308863dd8f
hamailfarooqui <hamail.farooqui@gmail.com>
2022-12-08 22:37:32 +0500

    Added-items-4

---
+++ frontend/src/data.js
@@ -0,0 +1,57 @@
+const data = {
+  products: [
+    {
+      name: 'Samsung Galaxy S22',
+      slug: 'samsung-galaxy-s22',
+      category: 'Mobiles',
+      image: '/images/p1.jpg',
+      price: 850,
+      countInStock: 8,
+      brand: 'Samsung',
+      rating: 4.7,
+      numReviews: 12,
+      desc: 'Samsung Galaxy s22 New Phone',
+    },
+
+    {
+      name: 'Samsung Galaxy Note 22',
+      slug: 'samsung-galaxy-note22',
+      category: 'Mobiles',
+      image: '/images/p2.jpg',
+      price: 1075,
+      countInStock: 12,
+      brand: 'Samsung',
+      rating: 4.8,
+      numReviews: 7,
+      desc: 'Samsung Galaxy Note 22 New Phone',
+    },
+
+    {
+      name: 'Iphone 14',
+      slug: 'iphone-14',
+      category: 'Mobiles',
+      image: '/images/p3.jpg',
+      price: 800,
+      countInStock: 22,
+      brand: 'Apple',
+      rating: 4.9,
+      numReviews: 17,
+      desc: 'Iphone 14 New Phone',
+    },
+
+    {
+      name: 'Google Pixel 7',
+      slug: 'google-pixel-7',
+      category: 'Mobiles',
+      image: '/images/p4.jpg',
+      price: 710,
+      countInStock: 11,
+      brand: 'Google',
+      rating: 4.4,
+      numReviews: 9,
+      desc: 'Google Pixel New Phone',
+    },
+  ],
+};
+
+export default data;
